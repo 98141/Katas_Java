b0045f0bae24348b25e01ff39f8f73c8324b3c01
Lina María Gallardo Corrales <linamgallardoc3@gmail.com>
2026-06-26 11:47:56 -0500

    Mejora ejercicio kata3

--- src/kata3.java
+++ src/kata3.java
@@ -5,10 +5,11 @@ public class kata3 {
         Scanner sc = new Scanner(System.in);
         int subTotal = 0;
         int precioIngresado = 1;
+        double descuento = 0;
         double IVA = 0;
         double total = 0;
         do {
-            System.out.println("Ingrese el precio de cada producto: ");
+            System.out.println("Ingrese el precio del producto (0 para terminar): ");
             precioIngresado = sc.nextInt();
             if (precioIngresado!=0 ){
                 subTotal = subTotal + precioIngresado;
@@ -17,12 +18,13 @@ public class kata3 {
         IVA = (subTotal*0.19);
         total = subTotal + IVA;
         if(total>50000){
-            double descuento = total*0.10;
+            descuento = total*0.10;
             total = total-descuento;
-            System.out.println("Descuento del 10%: "+ descuento);
         }
+        String mensaje = (descuento != 0 )? "Descuento del 10%: "+ descuento : ""; //operador ternario
         System.out.println("El total de la compra sin IVA es: "+ subTotal);
         System.out.println("El IVA sobre el total de su compra es: "+ IVA);
+        System.out.println(mensaje);
         System.out.println("El total de su compra con el IVA es: "+ total);
     }
 }
